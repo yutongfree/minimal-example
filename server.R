b45f52176360f6447c327231b58f0c6d1c911193
yutongfree <49895125+yutongfree@users.noreply.github.com>
2019-06-20 09:04:40 +0800

    Add files via upload

---
+++ server.R
@@ -0,0 +1,13 @@
+library(shiny) # load shiny at beginning at both scripts
+shinyServer(function(input, output) { # server is defined within
+  # these parentheses
+  output$textDisplay <- renderText({ # mark function as reactive
+    # and assign to
+    # output$textDisplay for
+    # passing to ui.R
+    paste0("You said '", input$comment,          # from the text
+           "'. There are ", nchar(input$comment),       # input control as
+           " characters in this."                                     # defined in ui.R
+    )
+  })
+})
